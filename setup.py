4ae482a95fa9b7ee880a14b7686b2382f38970ef
LoesjeNL <lwrutten@gmail.com>
2017-01-19 12:45:07 +0100

    more files

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='quirkyshopping',
+      version='1.0',
+      description='quirky shopping',
+      author='Louise Rutten',
+      author_email='lwrutten@gmail.com',
+      url='https://github.com/LoesjeNL/quirkyshopping.git',
+      scripts=['quirkyshopping']
+     )
